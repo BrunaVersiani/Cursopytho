9055a821b6ea1a74d35c702563339b92f609c344
RayaneVersiani <76855528+RayaneVersiani@users.noreply.github.com>
2022-08-22 17:00:48 -0300

    Estudando e praticando.

---
+++ ex051.py
@@ -0,0 +1,11 @@
+'''Exercício Python 51: Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre
+os 10 primeiros termos dessa progressão.'''
+print('='*20)
+print('TERMOS DE UMA PA')
+print('='*20)
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão: '))
+décimo = primeiro + (10 - 1) * razão
+for c in range(primeiro, décimo + razão, razão):
+    print('{} '.format(c), end=' - ')
+print('ACABOU')
