d911c320a382de03cd1730fca8249b8f823e3e18
RayaneVersiani <76855528+RayaneVersiani@users.noreply.github.com>
2022-08-22 16:05:47 -0300

    Estudando..

---
+++ ex047.py
@@ -0,0 +1,5 @@
+'''Exercício Python 47: Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.'''
+print('Contagem de numeros pares: ')
+for cont in range(2, 51, 2):
+    print(cont, end=' ')
+print('FIM')
