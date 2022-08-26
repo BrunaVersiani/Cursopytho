d9e21b83399673e80326fad1c72d30f41cfcfd09
RayaneVersiani <76855528+RayaneVersiani@users.noreply.github.com>
2022-08-25 23:10:42 -0300

    Atualizando fstrings

---
+++ ex067.py
@@ -0,0 +1,9 @@
+'''Exercício Python 67: Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor
+digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.'''
+cont = 0
+while True:
+    valor = int(input(('Digite um valor: '))
+    if valor < 0:
+        break
+        cont += 1
+        valor =
